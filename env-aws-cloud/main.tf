b569c8a2ae2241c1178f1e948a7b9d6966de6856
Odon Nkongolo <odonnkongolo@gmail.com>
2026-06-05 15:33:55 +0100

    fix: bypass openapi validation to prevent timeout

--- env-aws-cloud/main.tf
+++ env-aws-cloud/main.tf
@@ -57,6 +57,14 @@ resource "aws_security_group" "never404_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    description = "Kubernetes API Access"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # 3. Find the latest Ubuntu OS automatically
